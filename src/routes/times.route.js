b039df62b1212406823366f624cb68d99578aaa5
alvalenda <banysan@msn.com>
2022-08-03 16:11:59 -0300

    GET: findAllTimesByPosition reworked

--- src/routes/times.route.js
+++ src/routes/times.route.js
@@ -5,8 +5,8 @@ const timesController = require('../controllers/times.controller');
 router.get('/find_times', timesController.findAllTimesControler);
 /* GET_BY_ID */
 router.get('/find_time/:id', timesController.findByIdTimeController);
-/* GET_BY_POSITION */
-router.get('/find_tabela/:pos', timesController.findByPositionTimeController);
+/* GET_TABELA */
+router.get('/find_tabela', timesController.findAllTimesByPositionController);
 /* POST_TIME */
 router.post('/create', timesController.createTimeController);
 /* UPDATE_BY_ID */

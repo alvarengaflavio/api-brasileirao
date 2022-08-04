9c6699086bf4f5c933b3d9ea4987b30c381713c1
alvalenda <banysan@msn.com>
2022-08-04 19:15:48 -0300

    +TeamEntity => controller, services

--- src/controllers/times.controller.js
+++ src/controllers/times.controller.js
@@ -27,68 +27,53 @@ const findAllTimesByPositionController = (req, res) => {
 };
 
 const createTimeController = (req, res) => {
-  const time = req.body;
-  if (
-    !time ||
-    !time.time ||
-    !time.time.nome_popular ||
-    !time.time.sigla ||
-    !time.time.escudo ||
-    !time.vitorias ||
-    !time.empates ||
-    !time.derrotas ||
-    !time.gols_pro ||
-    !time.gols_contra
-  )
+  try {
+    const time = new timesService.TeamEntity(req.body);
+    time.validateTeam();
+    const newTime = timesService.createTimeService(time);
+    res.send(newTime);
+  } catch (err) {
     return res.status(400).send({
-      message: "You didin't fill all the required data fields",
+      message: err.message,
     });
-
-  const newTime = timesService.createTimeService(time);
-  res.send(newTime);
+  }
 };
 
 const updateTimeController = (req, res) => {
-  const editedId = Number(req.params.id);
+  try {
+    const editedTime = req.body;
+    const editedId = Number(req.params.id);
+    if (!editedId || isNaN(editedId))
+      throw new Error('An ID is required for this request.');
+    editedTime.time.time_id = editedId;
+    const editedEntity = new timesService.TeamEntity(editedTime);
+    editedEntity.validateTeam();
+    const updatedTime = timesService.updateTimeService(editedEntity);
 
-  if (!editedId)
-    return res
-      .status(400)
-      .send({ message: 'An ID is required for this request.' });
-
-  const editedTime = req.body;
-
-  if (
-    !editedTime ||
-    !editedTime.time ||
-    !editedTime.time.nome_popular ||
-    !editedTime.time.sigla ||
-    !editedTime.time.escudo ||
-    !editedTime.vitorias ||
-    !editedTime.empates ||
-    !editedTime.derrotas ||
-    !editedTime.gols_pro ||
-    !editedTime.gols_contra
-  )
+    res.send(updatedTime);
+  } catch (err) {
     return res.status(400).send({
-      message: "You didin't fill all the required data fields",
+      message: err.message,
     });
-
-  const updatedTime = timesService.updateTimeService(editedId, editedTime);
-  res.send(updatedTime);
+  }
 };
 
 const deleteTimeController = (req, res) => {
-  const idParam = Number(req.params.id);
+  try {
+    const idParam = Number(req.params.id);
+    if (!idParam || isNaN(idParam))
+      throw new Error('An ID is required for this request.');
 
-  if (!idParam)
-    return res
-      .status(400)
-      .send({ message: 'An ID is required for this request.' });
+    const deletedTime = timesService.deleteTimeService(idParam);
 
-  const deletedTime = timesService.deleteTimeService(idParam);
-  res.send(deletedTime);
+    res.send(deletedTime);
+  } catch (err) {
+    return res.status(400).send({
+      message: err.message,
+    });
+  }
 };
+// END OF CONTROLLERS
 
 module.exports = {
   findAllTimesControler,
--- src/services/times.service.js
+++ src/services/times.service.js
@@ -1,3 +1,4 @@
+const TeamEntity = require('../entities/team.entity');
 const times = require('../mocks/times');
 
 const findAllTimesService = () => {
@@ -9,79 +10,57 @@ const findByIdTimeService = (id) => {
 };
 
 const findAllTimesByPositionService = () => {
-  return timesSortedByPoints();
+  return TeamEntity.teamsSortedByPoints(times);
 };
 
 const createTimeService = (newTime) => {
-  const newId = findFreeId();
+  try {
+    newTime.id = TeamEntity.findFreeId(times);
+    newTime.updateTeamStats();
+    times.push(newTime.getTeam());
+    TeamEntity.updateTeamsPositions(times);
+    newTime.posicao = times[times.length - 1].posicao;
 
-  updateTimeStats(newTime, newId);
-  times.push(newTime);
-  updateTimesPositions();
-
-  return newTime;
+    return newTime.getTeam();
+  } catch (err) {
+    return err.message;
+  }
 };
 
-const updateTimeService = (id, timeEdited) => {
-  const timeIndex = times.findIndex((team) => team.time.time_id === id);
+const updateTimeService = (timeEdited) => {
+  try {
+    const timeIndex = times.findIndex(
+      (team) => team.time.time_id === timeEdited.id,
+    );
 
-  timeIndex !== -1
-    ? ((times[timeIndex] = timeEdited), updateTimeStats(times[timeIndex], id))
-    : (times.push(timeEdited), updateTimeStats(times[times.length - 1], id));
+    timeEdited.updateTeamStats();
+    timeIndex !== -1
+      ? (times[timeIndex] = timeEdited.getTeam())
+      : times.push(timeEdited.getTeam());
 
-  updateTimesPositions();
+    TeamEntity.updateTeamsPositions(times);
 
-  return times.find((time) => time.time.time_id === id);
+    return times.find((time) => time.time.time_id === timeEdited.id);
+  } catch (err) {
+    return err.message;
+  }
 };
 
 const deleteTimeService = (id) => {
-  const timeIndex = times.findIndex((team) => team.time.time_id === id);
-  const deletedTeam = times.splice(timeIndex, 1);
-  updateTimesPositions();
-  return deletedTeam;
+  try {
+    const timeIndex = times.findIndex((team) => team.time.time_id === id);
+
+    if (timeIndex === -1) throw new Error('ID not found.');
+
+    const deletedTeam = times.splice(timeIndex, 1);
+    TeamEntity.updateTeamsPositions(times);
+
+    return deletedTeam;
+  } catch (err) {
+    return err.message;
+  }
 };
 // END OF SERVICES
-
-// FUNCTIONS for manipulation of data structures
-const updateTimeStats = (team, id) => {
-  team.time.time_id = id;
-  team.saldo_gols = team.gols_pro - team.gols_contra;
-  team.pontos = team.vitorias * 3 + team.empates;
-  team.jogos = team.vitorias + team.empates + team.derrotas;
-  team.aproveitamento = Number((team.pontos / (team.jogos * 0.03)).toFixed(1));
-  team.variacao_posicao = 0;
-  team.ultimos_jogos = team.ultimos_jogos || [];
-};
-
-const updateTimesPositions = () => {
-  const sortedByPoints = timesSortedByPoints();
-  times.forEach((team) => {
-    team.posicao =
-      sortedByPoints.findIndex(
-        (s_team) => s_team.time.time_id === team.time.time_id,
-      ) + 1;
-  });
-};
-
-const timesSortedByPoints = () => {
-  return times.slice().sort((a, b) => b.pontos - a.pontos);
-};
-
-const findFreeId = () => {
-  const sortedById = times
-    .slice()
-    .sort((a, b) => a.time.time_id - b.time.time_id);
-  let previousId = 0;
-
-  for (const team of sortedById) {
-    if (team.time.time_id !== previousId + 1) {
-      return previousId + 1;
-    }
-    previousId = team.time.time_id;
-  }
-
-  return previousId + 1;
-};
 
 // EXPORTS
 module.exports = {
@@ -91,4 +70,5 @@ module.exports = {
   createTimeService,
   updateTimeService,
   deleteTimeService,
+  TeamEntity,
 };

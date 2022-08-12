36cf0c93c65c8e3bcf2b2266d02715abed8f510a
alvalenda <banysan@msn.com>
2022-08-11 23:42:14 -0300

    +default values

--- src/models/team.model.js
+++ src/models/team.model.js
@@ -1,29 +1,29 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const TeamSchema = new Schema({
-  posicao: { type: Number, required: false, default: undefined },
-  pontos: { type: Number, required: false },
+  posicao: { type: Number, default: undefined, required: false },
+  pontos: { type: Number, default: 0, required: false },
   time: {
-    nome_popular: { type: String, required: true, minLength: 4, maxLength: 15 },
+    nome_popular: { type: String, minLength: 3, maxLength: 16, required: true },
     sigla: {
       type: String,
-      required: true,
       minLength: 3,
       maxLength: 3,
       uppercase: true,
+      required: true,
     },
-    escudo: { type: String, required: true },
+    escudo: { type: String, maxLength: 60, required: true },
   },
-  jogos: { type: Number, required: false },
-  vitorias: { type: Number, required: true },
-  empates: { type: Number, required: true },
-  derrotas: { type: Number, required: true },
-  gols_pro: { type: Number, required: true },
-  gols_contra: { type: Number, required: true },
-  saldo_gols: { type: Number, required: false },
-  aproveitamento: { type: Number, required: false },
-  variacao_posicao: { type: Number, required: false },
-  ultimos_jogos: { type: Array, required: false, default: [] },
+  jogos: { type: Number, default: 0, required: false },
+  vitorias: { type: Number, default: 0, required: true },
+  empates: { type: Number, default: 0, required: true },
+  derrotas: { type: Number, default: 0, required: true },
+  gols_pro: { type: Number, default: 0, required: true },
+  gols_contra: { type: Number, default: 0, required: true },
+  saldo_gols: { type: Number, default: 0, required: false },
+  aproveitamento: { type: Number, default: 0, required: false },
+  variacao_posicao: { type: Number, default: 0, required: false },
+  ultimos_jogos: { type: Array, default: [], required: false },
 });
 
 const Team = model('teams', TeamSchema);
